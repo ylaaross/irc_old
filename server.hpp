c368fd237e8731df5133c6ad399296282c1ff3d1
yassir laarossi <130107186+ylaaross@users.noreply.github.com>
2024-07-13 19:36:30 +0100

    Hexchat works now
    Now i can send messages using hexchat and i can use messages inside channels and i can join multiple channels at the same time.

---
+++ server.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include "client.hpp"
+#include <map>
+#include <vector>
+// enums
+// {
+
+// }
+#define PRIVMSG_FORMAT(senderNick, senderUsername, senderHostname, receiver, message) ":" + senderNick + "!~" + senderUsername + "@" + senderHostname + " PRIVMSG " + receiver + " :" + message + "\r\n"
+#define RPL_JOIN(nick, username, channelname, ipaddress) ":" + nick + "!~" + username + "@" + ipaddress + " JOIN " + channelname + "\r\n"
+class server
+{
+	public:
+		std::map<int, client> clientServer;
+	public:
+		int channelMember();
+		void brodcast(std::string msg, std::string channel, int fd);
+		int channelname_used(std::string name);
+		void searchAdd(int fd, std::string ip);
+		int duplicatedNickname(std::string name);
+		void commandApply(int fd,  std::vector<std::string>commandLine, std::string password);
+		int searchForid(std::string name);
+		~server();
+};
+
+
+
